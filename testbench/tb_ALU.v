4cc37ec5fa813143cc63ec09da4c86a8573c814e
whbhw <whb6017@163.com>
2023-05-18 12:16:43 +0800

    newtb

--- testbench/tb_ALU.v
+++ testbench/tb_ALU.v
@@ -2,6 +2,7 @@
 `define MODULENAME ALU
 `define MODULENAMETB(X) TB_ALU``X
 `define VECNAME "../testbench/tb_ALU_vec.txt"
+`define ANSNAME "../testbench/tb_ALU_res.txt"
 `define ENDFLAG 32'hE0F
 `timescale 1ns/100ps
 module `MODULENAMETB(_SENDER) (
@@ -51,10 +52,45 @@ module `MODULENAMETB(_RECEIVER) (
     input   [31:0]  alu_res ,
     input           zero    
 );
+    reg [31:0]  messageans[0:10000];
+    integer     counter;
+    reg [31:0]  alu_res_ans;
+    reg         zero_ans;
+    reg         correctflag;
+    reg         percorrectflag;
+    reg         ready;
+    initial begin
+        $readmemh(`ANSNAME,messageans);
+        correctflag = 1'b1;
+        percorrectflag = 1'b1;
+        ready = 1'b0;
+        counter = 0;
+    end
     always @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
+            ready <= 1'b0;
+        end else if (!ready) begin
+            ready <= 1'b1;
+        end else if (messageans[counter] == `ENDFLAG) begin
+            $display("%b\n",correctflag);
         end else begin
-            $display("%H %B\n",alu_res,zero);
+            alu_res_ans = messageans[counter];
+            counter = counter + 1;
+            zero_ans = messageans[counter][0];
+            counter = counter + 1;
+            percorrectflag = 1'b1;
+            if ((alu_res ^ alu_res_ans)
+                    !== (alu_res_ans ^ alu_res_ans)) begin
+                percorrectflag = 1'b0;
+            end
+            if ((zero ^ zero_ans)
+                    !== (zero_ans ^ zero_ans)) begin
+                percorrectflag = 1'b0;
+            end
+            correctflag = correctflag & percorrectflag;
+            $display("%H%H    %b    %H%H\n",
+                    alu_res,zero,percorrectflag,
+                    alu_res_ans,zero_ans);
         end
     end
 endmodule
