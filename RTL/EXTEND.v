749f51ca0d9656f5fe4722f66af97eabaef25e33
whbhw <whb6017@163.com>
2023-05-22 18:36:12 +0800

    extend

--- RTL/EXTEND.v
+++ RTL/EXTEND.v
@@ -3,5 +3,6 @@ module EXTEND (
     input   wire    [15:0]  extend_in   ,
     input   wire            signext     
 );
-
+    assign extend_out[15:0] = extend_in;
+    assign extend_out[31:16] = signext ? {16{extend_in[15]}} : 16'b0;
 endmodule
